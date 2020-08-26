f1492ac08dd55c3e07a5619f28c09567b28900ed
mbaxask2 <sarah.kells@student.manchester.ac.uk>
2020-08-26 22:38:52 +0100

    UI Basic Layout, and simple functionality

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-var cookies = 0;
+var gold = 10;
 
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
+function getGold(number){
+    gold = gold + number;
+    document.getElementById("gold").innerHTML = gold;
 };
 
-var cursors = 0;
+var urchins = 0;
+var cost = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+function buy(obj){
+    var cost = Math.floor(10 * Math.pow(1.1,urchins));     //works out the cost of this cursor
+    if(gold >= cost){                                   //checks that the player can afford the cursor
+        urchins = urchins + 1;                                   //increases number of cursors
+    	gold = gold - cost;                          //removes the cookies spent
+        document.getElementById(obj+'Number').innerHTML = urchins;  //updates the number of cursors for the user
+        document.getElementById('gold').innerHTML = gold;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var cost = Math.floor(10 * Math.pow(1.1,urchins));       //works out the cost of the next cursor
+    document.getElementById(obj+'Cost').innerHTML = cost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 	
-	cookieClick(cursors);
+	getGold(urchins);
 	
 }, 1000);
