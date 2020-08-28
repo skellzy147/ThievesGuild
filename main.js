78d16f6babdc101f26045e75886119a04724ee7b
mbaxask2 <sarah.kells@student.manchester.ac.uk>
2020-08-28 15:09:58 +0100

    Move Game Logic to ThievesGuild Object and set up basic Faction Logic

--- main.js
+++ main.js
@@ -1,27 +1,54 @@
-var gold = 10;
+var ThievesGuild = {};
 
-function getGold(number){
-    gold = gold + number;
-    document.getElementById("gold").innerHTML = gold;
-};
+function Faction(name, cost, owned, mult) {
+    this.name = name;
+    this.cost = cost;
+    this.owned = owned;
+    this.mult = mult;
 
-var urchins = 0;
-var cost = 0;
+    this.setMult = function(mul) {
+        this.mult = mult;
+    } 
+
+    this.incOwned = function() {
+        this.owned+=1;
+    }
+}
+
+ThievesGuild.begin=function() {
+    ThievesGuild.gold = 0;
+    ThievesGuild.urchins = new Faction("Urchins", 0, 0, 0.0);
+    ThievesGuild.cost = 0;
+    ThievesGuild.ready = 1;
+
+    ThievesGuild.getGold = function(){
+        ThievesGuild.gold = ThievesGuild.gold + ThievesGuild.urchins.owned;
+        document.getElementById("gold").innerHTML = ThievesGuild.gold;
+        console.log(ThievesGuild.gold)    
+    }
+
+    ThievesGuild.buy = function(obj){
+        if(ThievesGuild.gold >= ThievesGuild.urchins.cost){                                   //checks that the player can afford the cursor
+            ThievesGuild.urchins.owned = ThievesGuild.urchins.owned + 1;                                   //increases number of cursors
+            ThievesGuild.gold = ThievesGuild.gold - ThievesGuild.urchins.cost;                          //removes the cookies spent
+            document.getElementById(obj+'Number').innerHTML = ThievesGuild.urchins.owned;  //updates the number of cursors for the user
+            document.getElementById('gold').innerHTML = ThievesGuild.gold;  //updates the number of cookies for the user
+        };
+        ThievesGuild.urchins.cost = Math.floor(10 * Math.pow(1.1,ThievesGuild.urchins.owned));       //works out the cost of the next cursor
+        document.getElementById(obj+'Cost').innerHTML = ThievesGuild.urchins.cost;  //updates the cursor cost for the user
+    };
+}
+
+ThievesGuild.begin();
 
 function buy(obj){
-    var cost = Math.floor(10 * Math.pow(1.1,urchins));     //works out the cost of this cursor
-    if(gold >= cost){                                   //checks that the player can afford the cursor
-        urchins = urchins + 1;                                   //increases number of cursors
-    	gold = gold - cost;                          //removes the cookies spent
-        document.getElementById(obj+'Number').innerHTML = urchins;  //updates the number of cursors for the user
-        document.getElementById('gold').innerHTML = gold;  //updates the number of cookies for the user
-    };
-    var cost = Math.floor(10 * Math.pow(1.1,urchins));       //works out the cost of the next cursor
-    document.getElementById(obj+'Cost').innerHTML = cost;  //updates the cursor cost for the user
-};
+    ThievesGuild.buy(obj)
+}
 
 window.setInterval(function(){
-	
-	getGold(urchins);
+    
+    if(ThievesGuild.ready !=0){
+        ThievesGuild.getGold();
+    }
 	
 }, 1000);
