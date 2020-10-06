c52497e415f30b389b5e0079cdf65de381961a10
mbaxask2 <sarah.kells@student.manchester.ac.uk>
2020-10-06 22:26:01 +0100

    F1 - Add Brutes

--- public/scripts/main.mjs
+++ public/scripts/main.mjs
@@ -10,17 +10,19 @@ var ThievesGuild = {};
 ThievesGuild.begin = function() {
   //* Game Variables
   ThievesGuild.tick =0
-  ThievesGuild.gold = 0;
+  ThievesGuild.gold = 1;
   ThievesGuild.Factions = [];
   ThievesGuild.cost = 0;
   ThievesGuild.ready = 1;
   ThievesGuild.drawn = 0;
 
   ThievesGuild.initializeFactionObjects = function() {
-    ThievesGuild.urchins = new Faction("urchins", 0, 0, 1.0, 1);
+    ThievesGuild.urchins = new Faction("urchins", 1, 0, 1.0, 1);
     ThievesGuild.pickpockets = new Faction("pickpockets", 50, 0, 1.0, 5);
+    ThievesGuild.brutes = new Faction("brutes", 150, 0, 1.0, 10);
     ThievesGuild.Factions.push(ThievesGuild.urchins);
     ThievesGuild.Factions.push(ThievesGuild.pickpockets);
+    ThievesGuild.Factions.push(ThievesGuild.brutes);
   };
 
   ThievesGuild.initializeFactionObjects();
